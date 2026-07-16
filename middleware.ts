4ac0499b89f0d23a3e02a17c81cf5c42d33883ca
Jaxon <enen.xmas@gmail.com>
2026-07-16 15:39:06 +0800

    fix: middleware typo

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,4 @@
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/request';
+import { NextResponse, NextRequest } from 'next/server';
 import { verifyToken } from './lib/auth';
 
 export async function middleware(request: NextRequest) {
